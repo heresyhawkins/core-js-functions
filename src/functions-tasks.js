cef9dc8b545ccb86ceae673ea3788321e07356d6
heresyhawkins <heresyhawkins@gmail.com>
2025-04-10 13:26:18 +0300

    feat: complete getArgumentsCount task

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -50,8 +50,9 @@ function getFunctionBody(func) {
  *  ]) => [0, 1, 2]
  *
  */
-function getArgumentsCount(/* funcs */) {
-  throw new Error('Not implemented');
+function getArgumentsCount(funcs) {
+  const array = funcs.map((func) => func.length);
+  return array;
 }
 
 /**
